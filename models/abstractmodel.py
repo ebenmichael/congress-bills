11262a644925507f84104aeed395e7921866219a
Eli Ben-Michael <ebenmichael@berkeley.edu>
2016-11-29 22:23:49 -0800

    Reconciled local/global parameter implementation with node implementation

--- models/abstractmodel.py
+++ models/abstractmodel.py
@@ -17,6 +17,31 @@ class AbstractModel(object):
         """Initialize variational parameters"""
         raise NotImplementedError
 
-    def get_nodes(self):
-        """Return a dictionary of (node_name, node)"""
-        return(self.nodes)
+    def calc_LP(self):
+        """Update the local parameters of the the model"""
+        for node_name in self.local_parameters:
+            self.nodes[node_name].vi_update()
+
+    def calc_SS(self):
+        """Compute the sufficient statistics for the global parameters"""
+        raise NotImplementedError
+
+    def calc_GP(self):
+        """Update the global parameters of the model"""
+        ss = self.calc_SS()
+        for node_name in self.global_parameters:
+            self.nodes[node_name].vi_update(ss)
+
+    def calc_elbo(self):
+        """Compute the current ELBO"""
+        elbo = 0
+        # compute node elbos
+        for node in self.nodes.values():
+            elbo += node.calc_elbo()
+        # compute data elbo
+        elbo += self.calc_data_elbo()
+        return(elbo)
+
+    def calc_data_elbo(self):
+        """Compute ELBO conrtibution from likelihood"""
+        raise NotImplementedError
