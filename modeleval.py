e66e622715df1a1bb51f942efcb8d017ee704d0d
Eli Ben-Michael <ebenmichael@berkeley.edu>
2016-12-04 01:23:40 -0800

    Fixed another small bug in modeleval, sorry for all the commits

--- modeleval.py
+++ modeleval.py
@@ -122,8 +122,7 @@ if __name__ == "__main__":
         caucus = np.loadtxt(sys.argv[3])
         caucus = np.dot(caucus, caucus.T)
         models = [LogisticRegression(C=1), LogisticRegression(C=0.1),
-                  LogisticRegression(C=0.01)]
-        """, IdealPointModel(1),
+                  LogisticRegression(C=0.01), IdealPointModel(1),
                   IdealPointModel(2)]
         for k in [1, 2, 3, 4, 10]:
             for s in [1, 2]:
@@ -131,5 +130,4 @@ if __name__ == "__main__":
 
         for sigma in [0.01, 0.1, 1, 100]:
             models.append(LCIPM(448, 2, 2, ip_prior_var=sigma))
-        """
         eval_and_save(models, interactions, membership=caucus, outdir=outdir)
